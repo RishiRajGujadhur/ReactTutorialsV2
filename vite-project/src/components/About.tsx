12341e388e80610ef85260c657b16af4016bedb5
Rishi Raj Gujadhur <r.r.gujadhur@gmail.com>
2024-10-18 19:03:07 +0400

    test

--- vite-project/src/components/About.tsx
+++ vite-project/src/components/About.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export function About() {
 
-  let [about, setAbout] = useState('Loading');
+  let [about, setAbout] = useState('Loading...');
   setAbout('Test');
   return (
     <>
