d04ff368115558654ba92b757e84d5c7b3c099e5
Rishi Raj Gujadhur <r.r.gujadhur@gmail.com>
2024-10-22 13:47:31 +0400

    use effect ts

--- vite-project/src/components/About.tsx
+++ vite-project/src/components/About.tsx
@@ -1,15 +1,24 @@
 import { useState,  useEffect } from "react";
 
 export function About() {
-  let [about, setAbout] = useState('Loading...');
-  
-  useEffect(() => {
-    setAbout('Hello World!');
+  let [about, setAbout] = useState<string>('Loading...');
+  const [color, setColor] = useState<string>();
+
+function handleColorChange(){
+  const nextColor = color === 'red' ? 'green' : 'red';
+  setColor(nextColor);
+}
+
+
+  useEffect(() => { 
+    setAbout('Hello World!');  
   }, []);
  
   return (
     <>
       <p>{about}</p>
+      <button onClick={handleColorChange}>Change Color</button>
+      <p>The current color is: {color}</p>
     </>
   );
 }
